9a17f98000a59e0604ab32392bdfd682cb0d64eb
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-09-16 12:05:17 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,20 +1,24 @@
 /**
- * The program shows how many logs
- *   can fit on a truck.
- *
+ * The program calculates the energy
+ *  using e=mmc^2
  * By:      Huzaifa Khalid
  * Version: 1.0
- * Since:   2022-09-14
+ * Since:   2022-11-25
  */
 
-import promptSync from "prompt-sync";
+import promptSync from 'prompt-sync'
 
-const prompt = promptSync();
+try {
+  const prompt = promptSync()
+  const massString = prompt('Enter the mass (in kg): ')
+  const massNumber = parseInt(massString)
+  if (isNaN(+massNumber)) {
+    throw new Error()
+  }
+  const energy = massNumber * (2.99 * 10 ** 8) ** 2
+  console.log(`${energy} joules of energy is released`)
+} catch (e) {
+  console.log('\nInvalid Input')
+}
 
-const mass = prompt("Ente mass(kg): ");
-const mass = Number(lengthString);
-const light = 2.998x10⁸;
-const energy = mass * light**2;
-console.log(`${energy} joules of energy`);
-
-console.log("\nDone.");
+console.log('\nDone.')
